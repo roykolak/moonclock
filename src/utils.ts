7a0db2140f2c24e88462392724f383146aba2b33
Roy Kolak <roykolak@fastmail.com>
2024-12-22 07:47:19 -0600

    time fix

--- src/utils.ts
+++ src/utils.ts
@@ -11,8 +11,12 @@ export function getEndDate(preset: Preset) {
     endDate.setDate(endDate.getDate() + preset.end.day);
     endDate.setHours(preset.end.hour, preset.end.minute, 0, 0);
   } else if (preset.mode === "offset") {
-    endDate.setHours(endDate.getHours() + preset.end.hour);
-    endDate.setMinutes(endDate.getMinutes() + preset.end.minute);
+    endDate.setHours(
+      endDate.getHours() + preset.end.hour,
+      endDate.getMinutes() + preset.end.minute,
+      0,
+      0
+    );
   }
 
   return endDate;
